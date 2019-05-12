7dd50689f719c2e419f6dd0db856863dc23f840f
Alexandre Fontoura <xesfnet@gmail.com>
2019-05-12 19:54:24 +0100

    Extracting BIG files from RESSOURC HQR entries

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const RESOURCES = [
 
 const BIG_FILES = [4, 9, 10, 11, 14];
 const BIG_FILES_STAGE8 = [4, 11, 14];
+const BIG_FILES_RESSOURC = [0, 1, 2, 26, 28, 31, 32, 33, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113];
 
 const dumpHQR = (group, filepath, name, stage) => {
     const fc = fs.readFileSync(path.join(datapath, filepath, name));
@@ -32,7 +33,8 @@ const dumpHQR = (group, filepath, name, stage) => {
         fs.writeFileSync(path.join(dumppathentry, `${name}_${e}.raw`), Buffer.from(data));
 
         if (stage !== 8 && BIG_FILES.includes(e) ||
-            stage === 8 && BIG_FILES_STAGE8.includes(e)) {
+            stage === 8 && BIG_FILES_STAGE8.includes(e) ||
+            stage === 'RESSOURC' && BIG_FILES_RESSOURC.includes(e)) {
             const bigEntries = loadBIG(data, entry.originalSize);
             for (let b = 0; b < bigEntries.length; b += 1) {
                 const bigEntry = bigEntries[b];
@@ -60,7 +62,8 @@ for (let r = 0; r < RESOURCES.length; r += 1) {
             dumpHQR('SCENES', path.join(stage, 'RUN1'), res.name, s);
         }
     } else {
-        dumpHQR(res.name.split('.')[0], '', res.name, -1);
+        const nameNoExt = res.name.split('.')[0];
+        dumpHQR(nameNoExt, '', res.name, nameNoExt);
     }
 }
 

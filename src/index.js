fcf178365f2be30b982f64b681221a157912e31c
Alexandre Fontoura <xesfnet@gmail.com>
2021-04-22 15:43:54 +0100

    Identify waveform, palettes and images files automatically.

--- src/index.js
+++ src/index.js
@@ -17,6 +17,35 @@ const BIG_FILES = [4, 9, 10, 11, 14];
 const BIG_FILES_STAGE8 = [4, 11, 14];
 const BIG_FILES_RESSOURC = [0, 1, 2, 26, 28, 31, 32, 33, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, /*103,*/ 104, 105, 106, 107, 108, 109, 110, 111, 112, 113];
 
+const isWaveform = (data) => {
+    const view = new DataView(data);
+    if (view.byteLength >= 4) {
+        const riff = view.getInt32(0, true);
+        if (riff === 1179011410) { // RIFF found
+            return true;
+        }
+    }
+    return false;
+};
+
+const getFileExtension = (data) => {
+    if (data.byteLength === 768) {
+        return 'pal';
+    } else if ( // not the best but to avoid creating a metadata file
+        data.byteLength === 19200 // texture 160x120
+        || data.byteLength === 65025 // texture 255x255
+        || data.byteLength === 65536 // shading palette image 
+        || data.byteLength === 76800 // image 320x240
+        || data.byteLength === 153600 // image 320x480
+        || data.byteLength === 307200 // image 640x480
+    ) {
+        return 'lim';
+    } else if (isWaveform(data)) {
+        return 'wav';
+    }
+    return 'raw';
+};
+
 const dumpHQR = (group, filepath, name, stage) => {
     const fc = fs.readFileSync(path.join(datapath, filepath, name));
     const buffer = fc.buffer.slice(fc.byteOffset, fc.byteOffset + fc.byteLength);
@@ -30,7 +59,8 @@ const dumpHQR = (group, filepath, name, stage) => {
         if (!fs.existsSync(dumppathentry)){
             fs.mkdirSync(dumppathentry, { recursive: true });
         }
-        fs.writeFileSync(path.join(dumppathentry, `${name}_${e}.raw`), Buffer.from(data));
+        const ext = getFileExtension(data);
+        fs.writeFileSync(path.join(dumppathentry, `${name}_${e}.${ext}`), Buffer.from(data));
 
         if (stage !== 8 && BIG_FILES.includes(e) ||
             stage === 8 && BIG_FILES_STAGE8.includes(e)) {
@@ -41,7 +71,7 @@ const dumpHQR = (group, filepath, name, stage) => {
                 if (!fs.existsSync(dumppathentrybig)){
                     fs.mkdirSync(dumppathentrybig, { recursive: true });
                 }
-                const ext = (e === 4) ? 'wav' : 'raw';
+                const ext = getFileExtension(data);
                 fs.writeFileSync(path.join(dumppathentrybig, `${name}_${e}_BIG_${b}.${ext}`), Buffer.from(bigEntry.data));
             }
         }
@@ -54,7 +84,7 @@ const dumpHQR = (group, filepath, name, stage) => {
                 if (!fs.existsSync(dumppathentrybig)){
                     fs.mkdirSync(dumppathentrybig, { recursive: true });
                 }
-                const ext = (e === 4) ? 'wav' : 'raw';
+                const ext = getFileExtension(data);
                 fs.writeFileSync(path.join(dumppathentrybig, `${name}_${e}_BIG_${b}.${ext}`), Buffer.from(bigEntry.data));
             }
         }

ffe2ac23229c00cf1e33ad94402373ee92cffba1
Alexandre Fontoura <xesfnet@gmail.com>
2021-04-23 09:21:01 +0100

    Convert raw images to bitmap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import os from 'os';
+import bmp from 'bmp-js';
 
 import { loadHQR, decompressHQR, loadBIG } from './hqr';
 
@@ -16,6 +17,8 @@ const RESOURCES = [
 const BIG_FILES = [4, 9, 10, 11, 14];
 const BIG_FILES_STAGE8 = [4, 11, 14];
 const BIG_FILES_RESSOURC = [0, 1, 2, 26, 28, 31, 32, 33, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, /*103,*/ 104, 105, 106, 107, 108, 109, 110, 111, 112, 113];
+
+let lastPaletteData = null;
 
 const isWaveform = (data) => {
     const view = new DataView(data);
@@ -46,6 +49,58 @@ const getFileExtension = (data) => {
     return 'raw';
 };
 
+const getImageSize = (data) => {
+    switch(data.byteLength) {
+        case 19200: // texture 160x120
+            return { w: 160, h: 120 };
+        case 65025: // texture 255x255
+            return { w: 255, h: 255 };
+        case 65536: // shading palette image 
+            return { w: 256, h: 256 };
+        case 76800: // image 320x240
+            return { w: 320, h: 240 };
+        case 153600: // image 320x480
+            return { w: 320, h: 480 };
+        case 307200: // image 640x480
+            return { w: 640, h: 480 };
+    }
+    return { w: 0, h: 0 };
+};
+
+const convertBuffer = (index, ext, data, filepath, filename) => {
+    if (ext === 'pal') {
+        lastPaletteData = new DataView(data);
+    } else if (ext === 'lim') { 
+        const view = new DataView(data);
+        const { w, h } = getImageSize(data);
+
+        const image = new Uint8Array(w * h * 4);
+        for (let x = 0; x < w; x++) {
+            for (let y = 0; y < h; y++) {
+                const rawOffset = (y * w + x);
+                const palOffset = view.getUint8(rawOffset, true);
+        
+                const r = lastPaletteData.getUint8(palOffset * 3, true);
+                const g = lastPaletteData.getUint8(palOffset * 3 + 1, true);
+                const b = lastPaletteData.getUint8(palOffset * 3 + 2, true);
+
+                const offset = rawOffset * 4;
+                image[offset    ] = 255; // alpha
+                image[offset + 1] = b;
+                image[offset + 2] = g;
+                image[offset + 3] = r;
+            }
+        }
+        const bmpData = {
+            data: image,
+            width: w,
+            height: h,
+        };
+        const rawData = bmp.encode(bmpData);
+        fs.writeFileSync(path.join(filepath, `${filename}.bmp`), rawData.data);
+    }
+};
+
 const dumpHQR = (group, filepath, name, stage) => {
     const fc = fs.readFileSync(path.join(datapath, filepath, name));
     const buffer = fc.buffer.slice(fc.byteOffset, fc.byteOffset + fc.byteLength);
@@ -61,6 +116,7 @@ const dumpHQR = (group, filepath, name, stage) => {
         }
         const ext = getFileExtension(data);
         fs.writeFileSync(path.join(dumppathentry, `${name}_${e}.${ext}`), Buffer.from(data));
+        convertBuffer(e, ext, data, dumppathentry, `${name}_${e}`);
 
         if (stage !== 8 && BIG_FILES.includes(e) ||
             stage === 8 && BIG_FILES_STAGE8.includes(e)) {
@@ -71,8 +127,9 @@ const dumpHQR = (group, filepath, name, stage) => {
                 if (!fs.existsSync(dumppathentrybig)){
                     fs.mkdirSync(dumppathentrybig, { recursive: true });
                 }
-                const ext = getFileExtension(data);
+                const ext = getFileExtension(bigEntry.data);
                 fs.writeFileSync(path.join(dumppathentrybig, `${name}_${e}_BIG_${b}.${ext}`), Buffer.from(bigEntry.data));
+                convertBuffer(b, ext, bigEntry.data, dumppathentrybig, `${name}_${e}_BIG_${b}`);
             }
         }
 
@@ -84,8 +141,9 @@ const dumpHQR = (group, filepath, name, stage) => {
                 if (!fs.existsSync(dumppathentrybig)){
                     fs.mkdirSync(dumppathentrybig, { recursive: true });
                 }
-                const ext = getFileExtension(data);
+                const ext = getFileExtension(bigEntry.data);
                 fs.writeFileSync(path.join(dumppathentrybig, `${name}_${e}_BIG_${b}.${ext}`), Buffer.from(bigEntry.data));
+                convertBuffer(b, ext, bigEntry.data, dumppathentrybig, `${name}_${e}_BIG_${b}`);
             }
         }
     }
